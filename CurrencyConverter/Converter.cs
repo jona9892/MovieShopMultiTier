0f6ad4a30f278b0359673e74788f4c3872e11d8a
h34dl3zz <bjorniz@hotmail.dk>
2015-11-09 13:28:38 +0100

    sup sup sup sup du er en kop ( og jeg er begyndt på currency converter)

---
+++ CurrencyConverter/Converter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    class Converter
+    {
+        public string ValutaAPIURL = "http://api.fixer.io/latest?base=DKK";
+    }
+}
